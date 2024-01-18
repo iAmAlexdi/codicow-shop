f863b65233666bdb322d8bf00f265f50651967be
MarcoMendza <marco.mendza@outlook.com>
2024-01-17 20:58:58 -0600

    Updated RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,10 @@ import { Providers } from "@/components";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - Teslo | Shop",
-    default: "Home - Teslo | Shop",
+    template: "%s - CodiCow | Shop",
+    default: "Home - CodiCow | Shop",
   },
-  description: "Una tienda virtual de productos",
+  description: "Tienda de demostracion de CodiCow",
 };
 
 export default function RootLayout({
